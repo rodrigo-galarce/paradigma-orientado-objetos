87639914b5498aab5a0bdf72d9db350d56ff99e9
Rodrigo Galarce <rgalarce@uade.edu.ar>
2026-04-06 17:22:57 -0300

    soluciona error comparator

--- src/LibroComparator.java
+++ src/LibroComparator.java
@@ -3,7 +3,7 @@ import java.util.Comparator;
 public class LibroComparator implements Comparator <Libro> {
     @Override
     public int compare(Libro l1, Libro l2) {
-        return l1.año.compareTo(l2.año);
+        return Integer.compare(l1.año, l2.año);
     }
 
 }
