3eb7750aad3a4a2a62a94264cc16cce507a48894
rodrigo-galarce <rgalarce@uade.edu.ar>
2026-04-18 19:08:38 -0300

    actualiza cambios

--- src/LibroComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class LibroComparator implements Comparator <Libro> {
-    @Override
-    public int compare(Libro l1, Libro l2) {
-        return Integer.compare(l1.año, l2.año);
-    }
-
-}
